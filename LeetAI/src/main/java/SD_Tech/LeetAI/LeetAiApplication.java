dd10d800d00c7370a4763047509e86fba4d8212e
PureGym0 <puregym000@gmail.com>
2025-11-08 23:35:32 +0530

    app propertirs

--- LeetAI/src/main/java/SD_Tech/LeetAI/LeetAiApplication.java
+++ LeetAI/src/main/java/SD_Tech/LeetAI/LeetAiApplication.java
@@ -4,11 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 @EnableAsync
 public class LeetAiApplication {
 
     public static void main(String[] args) {
+        // Manually load .env in case spring-dotenv fails
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing() // avoid crash if .env missing
+                .load();
+
+        // Force-set properties (Spring Boot will pick these up)
+        System.setProperty("DB_URL", dotenv.get("DB_URL", ""));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME", ""));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD", ""));
+
         SpringApplication.run(LeetAiApplication.class, args);
     }
 }
